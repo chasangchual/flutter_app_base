94902347f3abd7700d7eec576cba7bfe39bdacf5
Sangchual Cha <sangchual.cha@hotmail.com>
2024-10-25 23:44:04 -0400

    Add drawer with SideX widget

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
     return Obx(() {
       return GetMaterialApp(
         initialBinding: MainBindings(),
-        home: const MainScreen(),
+        home: MainScreen(),
         theme: lightTheme,
         darkTheme: darkTheme,
         themeMode: AppController.to.themeMode,
